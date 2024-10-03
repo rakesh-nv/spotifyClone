4e07517f42e94d647c7816a55068ce746c40c72d
rakesh <rakeshn2204@gmail.com>
2024-10-03 22:44:21 +0530

    fdf

--- Spotify-Clone-With-Flutter/lib/common/bloc/favorite_button/favorite_button_state.dart
+++
@@ -1,9 +0,0 @@
-abstract class FavoriteButtonState {}
-
-class FavoriteButtonInitial extends FavoriteButtonState {}
-
-class FavoriteButtonUpdated extends FavoriteButtonState {
-  final bool isFavorite;
-
-  FavoriteButtonUpdated({required this.isFavorite});
-}
--- Spotify-Clone-With-Flutter/lib/presentation/song_player/bloc/song_player_state.dart
+++
@@ -1,7 +0,0 @@
-abstract class SongPlayerState {}
-
-class SongPlayerLoading extends SongPlayerState {}
-
-class SongPlayerLoaded extends SongPlayerState {}
-
-class SongPlayerFailure extends SongPlayerState {}
